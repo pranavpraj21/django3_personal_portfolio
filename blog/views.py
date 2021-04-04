7f73e93d37eefdf0447f4d7bbedf53d79babcaf9
Pranav <pranavpraj21@gmail.com>
2021-04-04 19:19:39 +0530

    first one for portfolio

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render,get_object_or_404
+from .models import blog
+
+
+def all_blogs(request):
+    Blogs = blog.objects.order_by('-date')
+    return render(request,'blog/all_blogs.html',{'Blogs':Blogs})
+
+def detail(request, blog_id):
+    Blog = get_object_or_404(blog, pk=blog_id)
+    return render(request,'blog/detail.html',{'Blog':Blog})
